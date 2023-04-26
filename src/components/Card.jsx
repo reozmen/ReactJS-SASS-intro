06bed79d15e566e3c9eb87fc7b283a37d195f207
reozmen <recepozmen41@gmail.com>
2023-04-27 01:58:39 +0300

    Card.jsx changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,30 +2,50 @@ import cardStyle from "../scss/card.module.scss";
 
 //* data propsunun props olarak alıyoruz ve map fonksiyonu ile döngüye sokuyoruz.
 
-const Card = ({ data }) => {
+//** */ Birinci Yöntem- data propsunu alıyoruz ve map fonksiyonu ile döngüye sokuyoruz. Ve döngü içindeki her bir item ın içindeki name, job, comment, img, id yi destructuring ile alıyoruz. Ve componenete yazdırıyoruz
+// const Card = ({ data }) => {
+//   return (
+//     <div className={cardStyle["container"]}>
+//       {data.map((item) => {
+//         //* item ın içindeki name, job, comment, img, id yi destructuring ile alıyoruz.
+
+//         const { name, job, comment, img, id } = item;
+//         return (
+//           <div className={cardStyle["card"]} key={id}>
+//             <h1>{name}</h1>
+//             <h2>{job}</h2>
+//             <p>{comment}</p>
+//             <img src={img} alt="" />
+//             <div className={cardStyle["btn-container"]}>
+//               <button className={cardStyle["btn-container--small"]}>
+//                 Small
+//               </button>
+//               <button className={cardStyle["btn-container--large"]}>
+//                 Large
+//               </button>
+//             </div>
+//           </div>
+//         );
+//       })}
+
+//     </div>
+//   );
+// };
+
+//** İkinci Yöntem: data propsunun içindeki name, job, comment, img, id yi destructuring ile alıyoruz. Ve componenete yazdırıyoruz
+const Card = ({ name, job, comment, img, id }) => {
   return (
     <div className={cardStyle["container"]}>
-      {data.map((item) => {
-        //* item ın içindeki name, job, comment, img, id yi destructuring ile alıyoruz.
-
-        const { name, job, comment, img, id } = item;
-        return (
-          <div className={cardStyle["card"]} key={id}>
-            <h1>{name}</h1>
-            <h2>{job}</h2>
-            <p>{comment}</p>
-            <img src={img} alt="" />
-            <div className={cardStyle["btn-container"]}>
-              <button className={cardStyle["btn-container--small"]}>
-                Small
-              </button>
-              <button className={cardStyle["btn-container--large"]}>
-                Large
-              </button>
-            </div>
-          </div>
-        );
-      })}
+      <div className={cardStyle["card"]} key={id}>
+        <h1>{name}</h1>
+        <h2>{job}</h2>
+        <p>{comment}</p>
+        <img src={img} alt="" />
+        <div className={cardStyle["btn-container"]}>
+          <button className={cardStyle["btn-container--small"]}>Small</button>
+          <button className={cardStyle["btn-container--large"]}>Large</button>
+        </div>
+      </div>
     </div>
   );
 };
