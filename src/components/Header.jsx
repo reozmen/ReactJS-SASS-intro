e27c2fdc56c6310af8f2ce778b8f7619a123f127
reozmen <recepozmen41@gmail.com>
2023-04-27 01:41:01 +0300

    Header componentine açıklama eklendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
 import headerStyle from "../scss/header.module.scss";
 
+//* module css dosyalarını import etmek normal css dosyalarından farklıdır.
+//* module css dosyalarını import ederken dosyaya bir isim vermemiz gerekir.
+//* ve daha sonra bunu componenetin içinde kullanırken verdiğimiz isimle beraber kullanırız.
+//* örneğin yukarıda isim olarak headerStyle verdik ve bunu aşağıda kullanırken
+//* headerStyle["title"] şeklinde kullanırız.
+//* veya headerStyle.title şeklinde de kullanabiliriz.
+//* ama dosyamızdaki class isimlerinde tire(-) varsa . yerine [] kullanmamız gerekir.
+//* örneğin headerStyle["btn-container--small"] şeklinde kullanırız.
+//* headerStyle.btn-container--small şeklinde kullanamayız, hata alırız..
+
 const Header = () => {
   return (
     <header>
